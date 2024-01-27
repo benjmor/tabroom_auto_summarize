126b4c667d37879ca2f7925bda72bf62aa04e85c
Benjamin Morris <redbeardben@gmail.com>
2024-01-27 11:01:35 -0800

    getting closer to a working website

--- website/lambda/lambda_handler.py
+++ website/lambda/lambda_handler.py
@@ -7,15 +7,41 @@ def lambda_handler(event, context):
     print(event)
     # Do some data validation -- ensure that the number is 5 digits and the school name is 50 characters or less
     s3_client = boto3.client("s3")
-    file_path_to_find_or_create = event["queryStringParameters"]["file_path"]
+    parsed_body = json.loads(event["body"])
+    tournament_id = parsed_body["tournament"]
+    school_name = parsed_body["school"]
+    file_path_to_find_or_create = f"{tournament_id}/{school_name}/results.txt"
+    raw_gpt_submission = f"{tournament_id}/{school_name}/gpt_prompt.txt"
     bucket_name = os.environ["DATA_BUCKET_NAME"]
     file_exists = s3_client.list_objects_v2(
         Bucket=bucket_name, Prefix=file_path_to_find_or_create
     )
     if "Contents" in file_exists:
-        return {"statusCode": 200, "body": json.dumps("File exists")}
+        file_content = s3_client.get_object(
+            Bucket=bucket_name, Key=file_path_to_find_or_create
+        )
+        gpt_content = s3_client.get_object(
+            Bucket=bucket_name, Key=raw_gpt_submission
+        )
+        return {
+            "statusCode": 200,
+            "body": json.dumps(
+                {
+                    "file_content": file_content["Body"].read().decode("utf-8"),
+                    "gpt_content": gpt_content["Body"].read().decode("utf-8"),
+                }
+            ),
+        }
     else:
-        # TODO - At some point, test if the tournament results exist but the school is not present. That should be a failure returned to the user.
+        # Check if there are any files in the path bucket_name/tournament_id
+        all_objects=s3_client.list_objects_v2(
+            Bucket=bucket_name, Prefix=tournament_id,
+        )
+        if all_objects["KeyCount"] > 0:
+            return {
+                "statusCode": 200,
+                "body": json.dumps("Tournament exists, but school does not. Check that your school name matches the official name."),
+            }
         return {
             "statusCode": 200,
             "body": json.dumps("File does not exist, consider generating it"),
--- website/main.tf
+++ website/main.tf
@@ -1,16 +1,17 @@
 provider "aws" {
-  region = "us-east-1"  # Replace with your desired AWS region
+  region = "us-east-1" # Replace with your desired AWS region
 }
 
 locals {
-    website_bucket_name = "tabroom-summaries-website-bucket"
-    data_bucket_name = "tabroom-summaries-data-bucket"
+  website_bucket_name = "tabroom-summaries-website-bucket"
+  data_bucket_name    = "tabroom-summaries-data-bucket"
 }
 
 data "archive_file" "lambda_source" {
   type        = "zip"
   source_file = "${path.module}/lambda/lambda_handler.py"
   output_path = "${path.module}/lambda/lambda_handler.zip"
+
 }
 
 data "aws_iam_policy_document" "public_website_access" {
@@ -32,38 +33,67 @@ data "aws_iam_policy_document" "public_website_access" {
   }
 }
 
+data "aws_iam_policy_document" "lambda_s3_writes" {
+  statement {
+
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.data_bucket.arn,
+      "${aws_s3_bucket.data_bucket.arn}/*",
+    ]
+  }
+}
+
 # S3 Bucket for the website
 resource "aws_s3_bucket" "website_bucket" {
   bucket = local.website_bucket_name
 }
 
+resource "aws_s3_bucket_public_access_block" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_website_configuration" "website_bucket" {
-    bucket = aws_s3_bucket.website_bucket.id
+  bucket = aws_s3_bucket.website_bucket.id
 
   index_document {
     suffix = "index.html"
   }
 
-#   error_document {
-#     key = "error.html"
-#   }
+  #   error_document {
+  #     key = "error.html"
+  #   }
 }
 
 resource "aws_s3_object" "website_homepage" {
-  bucket = local.website_bucket_name
-  key    = "index.html"
-  source = "${path.module}/index.html"
-  etag = filemd5("${path.module}/index.html")
+  depends_on = [aws_s3_bucket.website_bucket]
+  bucket     = local.website_bucket_name
+  key        = "index.html"
+  source     = "${path.module}/index.html"
+  etag       = filemd5("${path.module}/index.html")
+
+  content_type = "text/html"
 }
 
 resource "aws_s3_bucket_policy" "public_access_to_website" {
-    bucket = aws_s3_bucket.website_bucket.id
-    policy = data.aws_iam_policy_document.public_website_access.json
+  depends_on = [aws_s3_bucket_public_access_block.website_bucket]
+  bucket     = aws_s3_bucket.website_bucket.id
+  policy     = data.aws_iam_policy_document.public_website_access.json
 }
 
 # S3 Bucket for the underlying data
 resource "aws_s3_bucket" "data_bucket" {
-    bucket = local.data_bucket_name
+  bucket = local.data_bucket_name
 }
 
 # IAM Role for Lambda function
@@ -84,9 +114,16 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-resource "aws_iam_policy" "lambda_policy" {
-    policy = "" # TODO - give it S3 read/write permissions
+resource "aws_iam_role_policy_attachment" "lambda_logging" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+resource "aws_iam_role_policy" "lambda_s3_writes" {
+  role   = aws_iam_role.lambda_role.id
+  policy = data.aws_iam_policy_document.lambda_s3_writes.json
+}
+
 
 # Lambda function to handle API Gateway requests
 resource "aws_lambda_function" "api_lambda_function" {
@@ -97,9 +134,10 @@ resource "aws_lambda_function" "api_lambda_function" {
   filename      = data.archive_file.lambda_source.output_path
   environment {
     variables = {
-        DATA_BUCKET_NAME=local.data_bucket_name
+      DATA_BUCKET_NAME = local.data_bucket_name
     }
   }
+  source_code_hash = data.archive_file.lambda_source.output_base64sha256
 }
 
 # API Gateway
@@ -115,7 +153,7 @@ resource "aws_api_gateway_rest_api" "website_api" {
 resource "aws_api_gateway_resource" "api_resource" {
   rest_api_id = aws_api_gateway_rest_api.website_api.id
   parent_id   = aws_api_gateway_rest_api.website_api.root_resource_id
-  path_part   = "{proxy+}"
+  path_part   = "submit_tournament"
 }
 
 resource "aws_api_gateway_method" "api_method" {
@@ -126,12 +164,12 @@ resource "aws_api_gateway_method" "api_method" {
 }
 
 resource "aws_api_gateway_integration" "api_integration" {
-  rest_api_id = aws_api_gateway_rest_api.website_api.id
-  resource_id = aws_api_gateway_resource.api_resource.id
-  http_method = aws_api_gateway_method.api_method.http_method
+  rest_api_id             = aws_api_gateway_rest_api.website_api.id
+  resource_id             = aws_api_gateway_resource.api_resource.id
+  http_method             = aws_api_gateway_method.api_method.http_method
   integration_http_method = "POST"
-  type        = "AWS_PROXY"
-  uri         = aws_lambda_function.api_lambda_function.invoke_arn
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.api_lambda_function.invoke_arn
 }
 
 resource "aws_lambda_permission" "api_lambda_permission" {
@@ -144,16 +182,16 @@ resource "aws_lambda_permission" "api_lambda_permission" {
 
 # Deploy the API Gateway
 resource "aws_api_gateway_deployment" "api_gateway_deployment" {
-  depends_on        = [aws_api_gateway_integration.api_integration]
-  rest_api_id       = aws_api_gateway_rest_api.website_api.id
-  stage_name        = "prod"
+  depends_on  = [aws_api_gateway_integration.api_integration]
+  rest_api_id = aws_api_gateway_rest_api.website_api.id
+  stage_name  = "prod"
 }
 
 # Outputs
 output "website_url" {
-  value = aws_s3_bucket.website_bucket.website_endpoint
+  value = aws_s3_bucket_website_configuration.website_bucket.website_endpoint
 }
 
 output "api_gateway_url" {
-  value = aws_api_gateway_deployment.api_gateway_url
+  value = aws_api_gateway_deployment.api_gateway_deployment.invoke_url
 }
