3b7ee9a75ae97a366e2b0b42f462b5529cba03b9
Benjamin Morris <redbeardben@gmail.com>
2024-03-20 14:15:33 -0700

    who names a school university anyway?

--- docker-deployment/tabroom_summary/scraper/resolve_longname_to_shortname.py
+++ docker-deployment/tabroom_summary/scraper/resolve_longname_to_shortname.py
@@ -59,6 +59,7 @@ def resolve_longname_to_shortname(long_name: str):
         "vegas debates": "Vegas Debates",
         "young genius, bay area speech and debate": "Young Genius",
         "university school": "University",
+        "university": "University",
         "greenwich hs": "GHS",
         "astor redhead academy": "FUHS",
         "stanford ohs": "Stanford OHS",
