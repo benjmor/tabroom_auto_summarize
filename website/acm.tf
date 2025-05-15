4c5c021bfbf22a5cca57a7297ac3fb3b12e72b3f
benjmor <44407400+benjmor@users.noreply.github.com>
2025-05-15 15:39:22 -0700

    fix circular validation reference

--- website/acm.tf
+++ website/acm.tf
@@ -8,14 +8,22 @@ resource "aws_acm_certificate" "cert" {
 
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
 
-resource "aws_route53_record" "cert_validation" {
-  count = length(aws_acm_certificate.cert.domain_validation_options)
-  name = element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_name, count.index)
-  type = element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_type, count.index)
-  zone_id = aws_route53_zone.exampleDomain.zone_id
-  records = [element(aws_acm_certificate.cert.domain_validation_options.*.resource_record_value, count.index)]
-  ttl = 60
+resource "aws_route53_record" "validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.exampleDomain.zone_id
 }
